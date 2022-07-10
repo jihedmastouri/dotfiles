8b52ed9f90b405df2e4f2071eed08319ffeb5541
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-07-10 03:13:43 +0100

    NVIM: First AUto Complete

--- .config/nvim/lua/plug.lua
+++ .config/nvim/lua/plug.lua
@@ -21,7 +21,10 @@ require('packer').startup(function()
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+
+  -- LUA SNIPPETS
   use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
 
   -- Work Tree View
   use 'ryanoasis/vim-devicons'
