92e4b843f24585058d9cba2eb16fae39fd3a8d8c
Jihed Mastouri <jihedmastouri.inbox@gmail.com>
2022-10-11 08:51:34 +0100

    Docker-Compose DB works

--- .config/nvim/after/plugin/lsp/cmp.lua
+++ .config/nvim/after/plugin/lsp/cmp.lua
@@ -11,7 +11,7 @@ local luasnip = require("luasnip")
 cmp.setup({
 	formatting = {
 		format = lspkind.cmp_format({
-			mode = "symbol",
+			mode = "text_symbol",
 			maxwidth = 50,
 			ellipsis_char = "...",
 		}),
@@ -26,7 +26,7 @@ cmp.setup({
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
